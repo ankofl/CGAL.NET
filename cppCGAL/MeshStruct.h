ba28fb1456cf016593b0dcd71230c9f44750548b
ankofl <ankofl@yandex.ru>
2024-03-13 09:14:20 +0300

    # cppCGAL deleted for CppCGAL

--- cppCGAL/MeshStruct.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-struct MeshStruct {
-    float* Points;
-    int* Indexes;
-    int PointsCount;  // Добавлено для хранения длины массива Points
-    int IndexesCount; // Добавлено для хранения длины массива Indexes
-};
